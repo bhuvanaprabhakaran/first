6d0c51cfc20a2efc7d8ada94541ad82def8cb7d1
bhuvanaprabhakaran <bhuvanabca41@gmail.com>
2023-12-11 15:21:57 +0530

    first commit

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -8,28 +8,123 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class FormComponent {
   newform = new FormGroup({
-    number : new FormControl()
-  })
-   data : any = []
+    number: new FormControl()
+  });
 
-   num1 :any ;
-   num2 :any ;
-   num3 : any ;
-   
-   onsubmit() {
+  data: any[] = [];
+
+  createFormGroup(): FormGroup {
+    return new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z]+$')]),
+      mobileno: new FormControl('', [Validators.required, Validators.pattern('^[0-9]{10}$')]),
+      address: new FormControl('', [Validators.required])
+    });
+  }
+
+  onsubmit() {
     let num = this.newform.controls.number.value;
     if (num <= 0 && this.data.length === 0) {
-      this.data = []; 
+      this.data = [];
       this.newform.reset();
-      return; 
+      return;
     } else {
       for (let i = 0; i < num; i++) {
-        this.data.push({});
-        this.newform.reset();
+        const newFormGroup = this.createFormGroup();
+        this.data.push({
+          num1: '',
+          num2: '',
+          num3: '',
+          submittedData: null,
+          showPopup: false,
+          form: newFormGroup
+        });
       }
     }
+    this.newform.reset();
   }
+
   onFormSubmit(item: any) {
-    item.submitted = true;
+    if (item.form.valid) {
+      item.submittedData = {
+        num1: item.form.value.name,
+        num2: item.form.value.mobileno,
+        num3: item.form.value.address
+      };
+      item.form.reset();
+    } else {
+      item.form.markAllAsTouched();
+    }
+  }
+
+  onEdit(index: number) {
+    const selectedItem = this.data[index];
+    selectedItem.num1 = selectedItem.submittedData.num1;
+    selectedItem.num2 = selectedItem.submittedData.num2;
+    selectedItem.num3 = selectedItem.submittedData.num3;
+  }
+
+
+  onUpdate(index: number) {
+    const selectedItem = this.data[index];
+    if (selectedItem.form.valid) {
+      selectedItem.submittedData = {
+        num1: selectedItem.form.value.name,
+        num2: selectedItem.form.value.mobileno,
+        num3: selectedItem.form.value.address
+      };
+      selectedItem.form.reset();
+      selectedItem.showPopup = false;
+    } else {
+      selectedItem.form.markAllAsTouched();
+    }
+  }
+
+  onDelete(index: number) {
+    const selectedItem = this.data[index];
+    selectedItem.submittedData = null;
+    selectedItem.showPopup = false;
+  }
+
+  showPopup(index: number) {
+    const selectedItem = this.data[index];
+    selectedItem.showPopup = true;
+  }
+
+  confirmDelete(index: number) {
+    const selectedItem = this.data[index];
+    selectedItem.submittedData = null;
+    selectedItem.showPopup = false;
+  }
+
+  cancelDelete(index: number) {
+    const selectedItem = this.data[index];
+    selectedItem.showPopup = false;
+  }
+  popup = false;
+  itemToDelete: any;
+
+  prepareDelete(item: any) {
+    this.itemToDelete = item;
+    this.showPopupform();
+  }
+
+  deleteForm() {
+    if (this.itemToDelete) {
+      const index = this.data.indexOf(this.itemToDelete);
+      if (index !== -1) {
+        this.data.splice(index, 1);
+        this.itemToDelete = null;
+      }
+    }
+    this.popup = false;
+  }
+
+  cancelForm() {
+    this.popup = false;
+  }
+  
+  showPopupform() {
+    this.popup = true;
+  }
+
 }
- }
